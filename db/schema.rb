26fe1293ff13714b572a7276f90f7670e02aab63
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-08 19:48:45 -0700

    added User and Restaurant models, as well as retaurants db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_192225) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_024623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
