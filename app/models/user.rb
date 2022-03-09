b816285695befdd75aecc08a97cb734e43f0c6cc
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-08 20:45:16 -0700

    added validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :reservations
     has_many :restaurants, through: :reservations
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true, length: {in: 2..20}
+    validates :password, length: {in: 6..10}
 end
