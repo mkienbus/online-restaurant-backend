fb4bf3ecba4137f64e5a2637b70ef511035ccc5e
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-14 20:40:00 -0600

    added reservations controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,16 @@
 class RestaurantsController < ApplicationController
     skip_before_action :authorize 
 
-    #GET /restaurants route
+    #GET /restaurants route for index
     def index
         restaurants = Restaurant.all
         render json: restaurants
+    end
+
+    #GET /restaurants route to show 
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant
     end
 
 end
