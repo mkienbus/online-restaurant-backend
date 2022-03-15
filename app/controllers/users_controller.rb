82706c27aea5ffb175a659bfc2dd78a202a04234
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-14 19:17:00 -0600

    added signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-    #skip_before_acton, only: create
-    skip_before_action
+    skip_before_action :authorize, only: :create
 
     # GET /users route
     def index
@@ -8,9 +7,28 @@ class UsersController < ApplicationController
         render json: users
     end
 
-    #GET route
-    def show
+     #GET route
+     def show
         render json: @current_user
+    end
+
+    #POST route for signup
+    def create
+        user = User.create(user_params)
+        session[:user_id] = user.id
+
+        if user.valid?
+        render json: user, status: :created
+
+        else
+            render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
     end
 
 end
