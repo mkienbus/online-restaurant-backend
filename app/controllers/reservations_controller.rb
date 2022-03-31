27b00efa141a56dea51502e585d98c7381942f12
Michael Kienbsuch <mkienbus@gmail.com>
2022-03-31 10:24:17 -0600

    minor change

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,6 +3,8 @@ class ReservationsController < ApplicationController
     
     #GET to /reservations route for index
     def index 
+        binding.irb
+        #@current_user.reservations to find all reservations for that logged in user
         reservations = Reservation.all
         render json: reservations
     end
