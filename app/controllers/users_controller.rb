d3ba2cbf406ecf1c3e318f5636c0913a2571c483
Michael Kienbsuch <mkienbus@gmail.com>
2022-04-04 09:30:29 -0600

    login/signup work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password)
+        params.permit(:username, :password, :password_confirmation)
     end
 
 end
