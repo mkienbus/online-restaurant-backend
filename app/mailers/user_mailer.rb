10ecbd438cc163bd6ef772ce605d0a6ff02a7378
Michael Kienbsuch <mkienbus@gmail.com>
2022-04-04 13:29:51 -0600

    building basic mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ApplicationMailer
-    default from: "notifications@example.com"
+    # default from: "notifications@example.com"
+    default from: "canonbusch@hotmail.com"
 
     def welcome_email
         @user = params[:user]
-        @url = 'http://localhost:4000/login'
+        @url = 'http://localhost:4000/restaurants'
         mail(to: @user.email, subject: 'Welcome to Rezy!')
     end
 end
